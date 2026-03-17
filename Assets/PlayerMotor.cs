e10dbc5afabbf7fa26aebed1a6b94761e43b8577
Kacper Prędkiewicz <kacper.predkiewicz@gmail.com>
2026-03-17 22:58:51 +0100

    New moving system

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -4,20 +4,48 @@ using UnityEngine.InputSystem;
 public class PlayerMotor : MonoBehaviour
 {
     Vector2 direction;
-    public float speed = 10;
+    public float acceleration = 10;
+    public float stoppingForce = 10;
+    public float maxSpeedX = 10;
+    public float stoppingPoint = 0.1f;
+    private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
-        transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * speed;
+        //accelerate if pressing button
+        if (direction.x != 0)
+        {
+            rb.AddForce(new Vector2(direction.x * acceleration, 0));
+        }
+        //if not accelerating start slowing down
+        else if (rb.linearVelocityX != 0)
+        {
+            //if almost stopped, force stop
+            if (rb.linearVelocityX < stoppingPoint && rb.linearVelocityX > -stoppingPoint)
+            {
+                rb.linearVelocity = new Vector2(0.0f, rb.linearVelocityY);
+            }
+            //add stopping force
+            else
+            {
+                rb.AddForce(new Vector2(-rb.linearVelocityX * stoppingForce, 0));
+            }
+        }
+
     }
 
     private void OnMove(InputValue value)
     {
         direction = value.Get<Vector2>();
+    }
+
+    private void OnJump()
+    {
+        rb.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
     }
 }
