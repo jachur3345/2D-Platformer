33c32e918ddcbd9c7e0a4d05fc534ad696f0efa2
Gawron10001 <kacper.predkiewicz@gmail.com>
2026-03-26 08:23:09 +0100

    Side step

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -5,11 +5,8 @@ using UnityEngine.InputSystem;
 public class PlayerMotor : MonoBehaviour
 {
     Vector2 direction;
-    public float acceleration = 10;
-    public float stoppingForce = 10;
-    public float maxSpeedX = 10;
-    public float stoppingPoint = 0.1f;
     public float jumpForce = 5;
+    public float speed = 5;
     private Rigidbody2D rb;
     private bool _canJump = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,36 +15,9 @@ public class PlayerMotor : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
-    private void FixedUpdate()
+    private void Update()
     {
-        //accelerate if pressing button
-        if (direction.x != 0)
-        {
-            rb.AddForce(new Vector2(direction.x * acceleration, 0));
-        }
-        //if not accelerating start slowing down
-        else if (rb.linearVelocityX != 0)
-        {
-            //if almost stopped, force stop
-            if (rb.linearVelocityX < stoppingPoint && rb.linearVelocityX > -stoppingPoint)
-            {
-                rb.linearVelocity = new Vector2(0.0f, rb.linearVelocityY);
-            }
-            //add stopping force
-            else
-            {
-                rb.AddForce(new Vector2(-rb.linearVelocityX * stoppingForce, 0));
-            }
-        }
-        //Limit max speed
-        if (rb.linearVelocityX >= maxSpeedX)
-        {
-            rb.linearVelocityX = maxSpeedX;
-        }
-        else if (rb.linearVelocityX <= -maxSpeedX)
-        {
-            rb.linearVelocityX = -maxSpeedX;
-        }
+        transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * speed;
     }
 
     private void OnMove(InputValue value)
