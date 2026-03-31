775ea7b1c51637696c7b1d303d9aca6e39d29a96
jachur3345 <dsw61100@student.dsw.edu.pl>
2026-03-31 04:05:59 +0200

    www

--- Assets/KillSpike.cs
+++ Assets/KillSpike.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 
 public class KillSpike : MonoBehaviour
@@ -5,12 +6,18 @@ public class KillSpike : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision);
+        Destroy(collision.gameObject);
     }
 }
