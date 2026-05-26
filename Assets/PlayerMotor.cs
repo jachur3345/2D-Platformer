da9bbe3c2f51527a1049d05272de3bd3f9d75fd4
jachur3345 <dsw61100@student.dsw.edu.pl>
2026-05-26 02:23:48 +0200

    nadrabianie

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -1,69 +1,130 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class PlayerMotor : MonoBehaviour
-{
-    Vector2 direction;
+{//////Vector2 direction;
+
+    public float dashForce = 10;
+
+    public float dashtime = 0.2f;
+
+    public float acceleration = 10;
+
     private Rigidbody2D rigidbody2D;
+
+    private Animator animator;
+
     private bool canJump = true;
+
     public float maxSpeed = 10;
-    public float stoppingForce = 5;
+
+    public float stoppingForce = 10;
+
     public float speed = 10;
+
     public float jumpForce = 10;
 
-    private int _JumpCount = 0;
+    private bool isDashing = false;
+
+
+    private int jumpCount = 0;
+
     public int maxJumpCount = 2;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void Start()
+
+    private float initXScale;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is createdprivate void Start(){        rigidbody2D = GetComponent<Rigidbody2D>();
+
+    animator = GetComponent<Animator>();
+
+        initXScale = transform.localScale.x;
+
+    }// Update is called once per frameprivate void FixedUpdate(){        
+
+rigidbody2D.AddForce(new Vector2(direction.x * speed, 0));
+
+
+if (isDashing)
+{
+    return;
+
+}//Limit max speedif (rigidbody2D.linearVelocityX >= maxSpeed){            rigidbody2D.linearVelocityX = maxSpeed;
+
+        }else if (rigidbody2D.linearVelocityX <= -maxSpeed)
+{
+    rigidbody2D.linearVelocityX = -maxSpeed;
+
+}
+if (direction.x == 0 && rigidbody2D.linearVelocityX != 0)
+{
+    rigidbody2D.AddForce(new Vector2(-rigidbody2D.linearVelocityX * stoppingForce, 0));
+
+}
+if (direction.x != 0)
+{
+    animator.SetBool("IsMoving", true);
+
+}
+else
+{
+    animator.SetBool("IsMoving", false);
+
+}
+if (direction.x > 0)
+{
+    transform.localScale = new Vector3(initXScale, transform.localScale.y, transform.localScale.z);
+
+}
+else if (direction.x < 0)
+{
+    transform.localScale = new Vector3(-initXScale, transform.localScale.y, transform.localScale.z);
+
+}}private void OnMove(InputValue value)
+{
+    direction = value.Get<Vector2>();
+
+}
+private void OnJump()
+{
+    if (canJump)
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
-    }
-    // Update is called once per frame
-    private void FixedUpdate()
-    {
-        rigidbody2D.AddForce(new Vector2(direction.x * speed, 0));
+        rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 
-        if (rigidbody2D.linearVelocityX >= maxSpeed)
-        {
-            rigidbody2D.linearVelocityX = maxSpeed;
-        }
+        jumpCount++;
 
-        else if (rigidbody2D.linearVelocityX <= -maxSpeed)
-        {
-            rigidbody2D.linearVelocityX = -maxSpeed;
-        }
+        if (jumpCount >= maxJumpCount) canJump = false;
 
-        if (direction.x == 0 && rigidbody2D.linearVelocityX != 0)
-        {
-            rigidbody2D.AddForce(new Vector2(-rigidbody2D.linearVelocityX * stoppingForce, 0));
-        }
-
-
-    }
-
-    private void OnMove(InputValue value)
-    {
-        direction = value.Get<Vector2>();
-    }
-
-    private void OnJump()
-    {
-        if (canJump)
-        {
-            rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            _JumpCount++;
-            if(_JumpCount >= maxJumpCount)
-
-            canJump = false;
-        }
-
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        canJump = true;
-        _JumpCount = 0;
     }
 }
+private void OnCollisionEnter2D(Collision2D collision)
+{
+    canJump = true;
+
+    jumpCount = 0;
+
+}
+private void OnDash()
+{
+
+    isDashing = true;
+
+    //Debug.Log("Dashing");        rigidbody2D.AddForce(new Vector2(direction.x * dashForce, 0), ForceMode2D.Impulse);
+
+    StartCoroutine(ResetDash(dashtime));
+
+
+
+
+}
+IEnumerator ResetDash(float timeToRest)
+{
+    yield return new WaitForSeconds(timeToRest);
+
+    isDashing = false;
+
+}}
